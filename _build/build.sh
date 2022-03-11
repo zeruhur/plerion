61dbad68ec0492aec1ecb20b5213bde91bd9037a
zeruhur <51272598+zeruhur@users.noreply.github.com>
2022-03-11 16:42:10 +0100

    Update build.sh

--- _build/build.sh
+++ _build/build.sh
@@ -1,2 +1,2 @@
-pandoc -f gfm -t html -o pdf/plerion.pdf -s md/plerion.md --pdf-engine=weasyprint --css="css/plerion.css" --metadata pagetitle="Plerion" --columns=2
+pandoc -f gfm -t html -o pdf/plerion_ashcan.pdf -s md/plerion.md --pdf-engine=weasyprint --css="css/plerion.css" --metadata pagetitle="Plerion" --columns=2
 #--author="CC-BY-SA-4.0 Roberto Bisceglie"
