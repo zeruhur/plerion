e45faa798993d3e254344f085ba9c31e02dbcb06
zeruhur <51272598+zeruhur@users.noreply.github.com>
2022-10-06 17:47:48 +0200

    Update build.sh

--- _build/build.sh
+++ _build/build.sh
@@ -1,3 +1,4 @@
 for i in ../[0]*.md ; do $(pandoc -f gfm -t html -o html/$(basename ${i} .md).html $i) ; done
 pandoc -t html -o pdf/plerion_ashcan.pdf -s html/*.html character_sheet.html --toc --toc-depth=1 --css="css/plerion.css" --metadata-file=metadata.yml --template=template/template.html --pdf-engine=weasyprint
+# pandoc -t rtf - o text/plerion.rtf -s html/*.html
 rm html/*.html
